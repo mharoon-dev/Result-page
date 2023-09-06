fba02abe90fa34e90c5f4f2b4079b060f3508b20
haroon124 <agar3d@gmail.com>
2023-09-06 18:33:40 +0200

    result page

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const input = document.getElementById("input")
 
+
 function searchBtn() {
     if (input.value == "133749") {
-        window.location = "./result page/results/result1.html"
+        window.location = "./result page/results/result 1.html"
     } else if (input.value == "130474" ) {
         window.location = "./result page/results/result 2.html"
     } else if (input.value == "128" ) {
         window.location = "./result page/results/result 3.html"
-    } else if (input.value == "133750" ) {
+    } else if (input.value == "131456" ) {
         window.location = "./result page/results/result 4.html"
+    } else if (input.value == " " ) {
+        window.location = "./result page/results/result 5.html"
+    } else if (input.value == "136302" ) {
+        window.location = "./result page/results/result 6.html"
+    } else if (input.value == "130005" ) {
+        window.location = "./result page/results/result 7.html"
+    } else if (input.value == "131496" ) {
+        window.location = "./result page/results/result 8.html"
+    } else if (input.value == "137959" ) {
+        window.location = "./result page/results/result 9.html"
+    } else if (input.value == "132576" ) {
+        window.location = "./result page/results/result 10.html"
     } else {
         alert("Write the correct roll number")
     }
 }
+
+
+  
